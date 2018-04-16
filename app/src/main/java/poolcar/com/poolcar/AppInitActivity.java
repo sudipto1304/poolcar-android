3e11ca4685088c1d46d4f6bf54593e3b9529d8a9
FORDNA1\SAICH1 <saich1@ford.com>
2018-04-16 15:55:04 -0400

    Inti Application

---
+++ app/src/main/java/poolcar/com/poolcar/AppInitActivity.java
@@ -0,0 +1,13 @@
+package poolcar.com.poolcar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppInitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_init);
+    }
+}
