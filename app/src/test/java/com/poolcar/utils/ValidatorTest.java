b37e74245469876c7bb545abc86a71101e1899d7
FORDNA1\SAICH1 <saich1@ford.com>
2018-04-20 13:30:04 -0400

    creating notification

--- app/src/test/java/com/poolcar/utils/ValidatorTest.java
+++ app/src/test/java/com/poolcar/utils/ValidatorTest.java
@@ -8,15 +8,19 @@ import org.junit.Test;
 public class ValidatorTest {
 
 
-    Validator validator;
-    private String[] validEmail = new String[] { "mkyong@yahoo.com",
+    static Validator validator;
+    private static String[] validEmail = new String[] { "mkyong@yahoo.com",
             "mkyong-100@yahoo.com", "mkyong.100@yahoo.com",
             "mkyong111@mkyong.com", "mkyong-100@mkyong.net",
             "mkyong.100@mkyong.com.au", "mkyong@1.com",
             "mkyong@gmail.com.com", "mkyong+100@gmail.com",
-            "mkyong-100@yahoo-test.com" };
+            "mkyong-100@yahoo-test.com","mkyong", "mkyong@.com.my",
+            "mkyong123@gmail.a", "mkyong123@.com", "mkyong123@.com.com",
+            ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
+            "mkyong..2002@gmail.com", "mkyong.@gmail.com",
+            "mkyong@mkyong@gmail.com", "mkyong@gmail.com.1a", "sudipto@gmail.co.in" };
 
-    private String[] invalidEmail = new String[] { "mkyong", "mkyong@.com.my",
+    private static String[] invalidEmail = new String[] { "mkyong", "mkyong@.com.my",
             "mkyong123@gmail.a", "mkyong123@.com", "mkyong123@.com.com",
             ".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
             "mkyong..2002@gmail.com", "mkyong.@gmail.com",
@@ -24,7 +28,7 @@ public class ValidatorTest {
 
 
     @BeforeClass
-    public void initData() {
+    public static void initData() {
         validator = new Validator();
     }
 
@@ -35,7 +39,7 @@ public class ValidatorTest {
         for (String temp : validEmail) {
             boolean valid = validator.validateEmailId(temp);
             System.out.println("Email is valid : " + temp + " , " + valid);
-            Assert.assertEquals(valid, true);
+            Assert.assertTrue(valid);
         }
 
     }
@@ -46,7 +50,7 @@ public class ValidatorTest {
         for (String temp : invalidEmail) {
             boolean valid = validator.validateEmailId(temp);
             System.out.println("Email is valid : " + temp + " , " + valid);
-            Assert.assertEquals(valid, false);
+            Assert.assertFalse(valid);
         }
     }
 
