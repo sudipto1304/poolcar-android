c4b864df5a73b2af3ce74e8ff3c7355de8c17ce0
Sudipto Aich <>
2018-05-17 09:09:44 -0400

    BDD implementation

--- app/src/androidTest/java/com/poolcar/test/features/RegistrationTest.java
+++ app/src/androidTest/java/com/poolcar/test/features/RegistrationTest.java
@@ -2,31 +2,19 @@ package com.poolcar.test.features;
 
 import com.poolcar.R;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.Matchers.startsWith;
 
 public class RegistrationTest {
-
-
-
-
-
-
-
+    
 
     @Given("^user will see slider view$")
     public void user_will_see_slider_view() throws Throwable {
@@ -43,12 +31,13 @@ public class RegistrationTest {
     @Then("^Get Started button will be displayed$")
     public void get_Started_button_will_be_displayed() throws Throwable {
         Thread.sleep(2000);
-        onView(allOf(withText("Get Started"))).perform(click());
-        //onView(withId(R.id.getStartedButton)).perform(click());
+
+
     }
 
     @Then("^user click on Get Started button$")
     public void user_click_on_Get_Started_button() throws Throwable {
+        onView(allOf(withId(R.id.getStartedButton), isDisplayed())).perform(click());
 
     }
 
@@ -61,7 +50,7 @@ public class RegistrationTest {
 
     @When("^user will click on Create an Account link$")
     public void user_will_click_on_Create_an_Account_link() throws Throwable {
-
+        onView(withId(R.id.createAccount)).perform(click());
     }
 
 
@@ -70,4 +59,6 @@ public class RegistrationTest {
 
 
     }
+
+
 }
